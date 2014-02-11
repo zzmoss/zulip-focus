ef395b33ebca566f5e80a25accfc097945bb3409
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-02-11 15:59:36 -0500

    First commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject zulip-focus "0.0.1"
+        :description "Zulip bot that helps filter and view content from specific streams and topics"
+        :dependencies [[org.clojure/clojure "1.5.1"] [clj-http "0.7.9"]]
+        :main zulip-focus.bot)
+
---
+++ src/zulip_focus/bot.clj
@@ -0,0 +1,7 @@
+(ns zulip-focus.bot
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))   
